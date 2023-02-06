005b803970879f1fbe52f350e5305dc615f05a56
Christian Bongard <christian.bongard90@gmail.com>
2023-02-06 21:10:27 +0400

    added the show

--- app.rb
+++ app.rb
@@ -4,5 +4,26 @@ require "sinatra/reloader"
 require "sinatra/activerecord"
 
 get "/" do
-  "Hello world!"
+  @restaurants = Restaurant.all
+  # all instances: index
+  erb :index # embedded Ruby (HTML + Ruby)
+end
+
+get '/restaurants/:id' do
+  id = params[:id]
+  @restaurant = Restaurant.find(id)
+  # single page: show
+  erb :show
+end
+
+# get: receive from the server
+# post: send data to the server
+
+# Create a form
+post '/restaurants' do
+  name = params[:name]
+  city = params[:city]
+
+  Restaurant.create(name: name, city: city)
+  redirect '/' # redirect to the homepage
 end
